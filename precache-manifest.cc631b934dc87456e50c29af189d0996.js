cd8cb76becc4a8b8b41047834f157d7cf4475f09
Marco <mf.hermel@gmail.com>
2019-10-25 16:11:10 -0300

    Updates

---
+++ precache-manifest.cc631b934dc87456e50c29af189d0996.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c518ee0ecda89ce7e326253079633b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e08f68f7d071dd81745e",
+    "url": "/static/css/main.9b926ca9.chunk.css"
+  },
+  {
+    "revision": "aed0da3d330e99eeac6c",
+    "url": "/static/js/2.f2b987ea.chunk.js"
+  },
+  {
+    "revision": "e08f68f7d071dd81745e",
+    "url": "/static/js/main.202a0c71.chunk.js"
+  },
+  {
+    "revision": "583375f41a9071480c69",
+    "url": "/static/js/runtime-main.9936206a.js"
+  },
+  {
+    "revision": "1fcf6e20e04d591ed9f8427322ad78ae",
+    "url": "/static/media/Cinzel-Regular.1fcf6e20.otf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "a177ec339363e260702c460b62ca7137",
+    "url": "/static/media/faixa.a177ec33.svg"
+  },
+  {
+    "revision": "9b965feb22f61084d98feed0b6f5f82d",
+    "url": "/static/media/logo.9b965feb.svg"
+  },
+  {
+    "revision": "435cade88f9ded2c6b7ee440bb76d3b4",
+    "url": "/static/media/stars.435cade8.jpg"
+  }
+]);
